f33b9d99a1d731e72117c28992067cd76f90efd7
Adeniji Victor Adeolu <adenijivictor99.va@gmail.com>
2023-09-26 19:00:27 +0100

    nav fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -26,11 +26,12 @@ export const Navbar = () => {
                         }
                 </button>
 
-                <ul className={`sm:flex sm:gap-6 px-9 shadow-md md:shadow-none bg-[#161927] md:bg-transparent
+                <ul className={`${isOpen? "left-0 ": "-left-[400%] "}
+                                sm:flex sm:gap-6 px-9 shadow-md md:shadow-none bg-[#161927] md:bg-transparent
                                 md:flex md:items-center py-4
                                 absolute top-[4.7rem] md:static z-[100] md:z-auto   
                                 w-full md:w-auto md:pl-0 space-y-7 sm:space-y-0 md:space-y-0 left-0 transition-all duration-150
-                                ${isOpen? "left-0 ": "-left-[400%] hidden "}`
+                                `
                             }
             >
                     <li>
